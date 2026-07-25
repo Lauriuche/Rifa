3720c4c054bfd46f8e90c280870bbe0f7a331f90
Lauriuche <lauriuche@gmail.com>
2026-07-25 20:06:08 -0300

    Add files via upload

---
+++ server.js
@@ -0,0 +1,16 @@
+{
+"name": "rifa-master-backend",
+"version": "1.0.0",
+"description": "API Backend para SaaS Rifa Master",
+"main": "server.js",
+"scripts": {
+"start": "node server.js"
+},
+"dependencies": {
+"cors": "^2.8.5",
+"dotenv": "^16.4.5",
+"express": "^4.19.2",
+"firebase-admin": "^12.1.0",
+"mercadopago": "^2.0.9"
+}
+}
