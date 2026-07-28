3c36ce77c9231aa855e767c3a9ee8f3231e67401
Lauriuche <lauriuche@gmail.com>
2026-07-28 12:58:07 -0300

    Update and rename se.js to sw.js

---
+++ sw.js
@@ -0,0 +1,17 @@
+// Mude este número da versão sempre que quiser forçar uma atualização no celular dos clientes!
+const CACHE_VERSION = 'v1.0.0'; 
+
+self.addEventListener('install', (event) => {
+    console.log('Service Worker instalado: ', CACHE_VERSION);
+});
+
+self.addEventListener('fetch', (event) => {
+    // O site carrega normalmente
+});
+
+// AQUI ESTÁ O SEGREDO: Ouve o clique do botão "Atualizar"
+self.addEventListener('message', (event) => {
+    if (event.data && event.data.type === 'SKIP_WAITING') {
+        self.skipWaiting(); // Força a nova versão a assumir o controle
+    }
+});
