d7dabef2eaf4b130559943adabce93c93927afab
Lauriuche <lauriuche@gmail.com>
2026-07-28 13:10:59 -0300

    Update CACHE_VERSION to v1.0.1

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 // Mude este número da versão sempre que quiser forçar uma atualização no celular dos clientes!
-const CACHE_VERSION = 'v1.0.0'; 
+const CACHE_VERSION = 'v1.0.1'; 
 
 self.addEventListener('install', (event) => {
     console.log('Service Worker instalado: ', CACHE_VERSION);
